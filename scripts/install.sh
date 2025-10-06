b3735a8acd0a7809fb2933f53dad1e0807442d86
John Hass <john8675309@gmail.com>
2025-10-06 17:21:30 -0500

    install.sh Updates

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 apt update
-apt install qemu-utils nfs-common whiptail iw net-tools dmidecode pv wpasupplicant grub-efi-amd64-bin grub-efi-amd64-signed
+apt install qemu-utils nfs-common whiptail iw net-tools dmidecode pv wpasupplicant grub-efi-amd64-bin grub-efi-amd64-signed nfs-kernel-server
